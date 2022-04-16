dd51451afae7a6484fe8cd80797cbee27df96229
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2022-04-16 18:55:26 -0300

    refactor: Movendo classe ResponseMessage para o core

---
+++ BFF.Core/Messages/ResponseMessage.cs
@@ -0,0 +1,36 @@
+﻿
+using BFF.Core.Validators;
+
+namespace BFF.Core.Messages
+{
+    public class ResponseMessage<T>
+    {
+        public ValidationResult ValidationResult { get; private set; }
+        public T Data { get; set; }
+
+        public ResponseMessage(ValidationResult validationResult)
+        {
+            ValidationResult = validationResult;
+        }
+
+        public ResponseMessage(T data)
+        {
+            ValidationResult = new ValidationResult();
+            Data = data;
+        }
+
+    }
+
+    public static class ResponseMessageFactory
+    {
+        public static ResponseMessage<T> GerarErrorResponse<T>(ValidationResult validationResult)
+        {
+            return new ResponseMessage<T>(validationResult);
+        }
+
+        public static ResponseMessage<T> GerarResponse<T>(T data)
+        {
+            return new ResponseMessage<T>(data);
+        }
+    }
+}
